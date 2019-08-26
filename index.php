e42c685db49706cd55cd6c98b5703a93673ec4ad
CYRIL VELLA <cyril.vella@yahoo.com>
2019-08-26 08:24:55 +0200

    first comiit

---
+++ index.php
@@ -0,0 +1,414 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+	<title>Association - Arcades Vitrolles</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="apple-touch-icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css" id="color-switcher-link">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
+
+<body class="home">
+
+
+	<!-- wrappers for visual page editor and boxed version of template -->
+	<div id="canvas">
+		<div id="box_wrapper">
+
+			<!-- Header -->
+			<header class="page_header">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="block-header1 text-left">
+								<div class="header-search">
+									<a href="index.php#" class="search_modal_button sh-button">
+										<i class="fa fa-search" aria-hidden="true"></i>
+									</a>
+								</div>
+								<div class="social-list">
+									<span class="social-list__text">Réseaux sociaux</span>
+									<a href="index.php#" class="fa fa-facebook" aria-hidden="true"></a>
+									<a href="index.php#" class="fa fa-twitter" aria-hidden="true"></a>
+									<a href="index.php#" class="fa fa-youtube-play" aria-hidden="true"></a>
+									<a href="index.php#" class="fa fa-google-plus" aria-hidden="true"></a>
+									<a href="index.php#" class="fa fa-vimeo" aria-hidden="true"></a>
+								</div>
+							</div>
+							<div class="block-header2 text-center">
+								<!-- main nav start -->
+								<nav class="mainmenu_wrapper">
+									<ul class="mainmenu nav sf-menu">
+										<li class="active">
+											<a href="index.php">Accueil</a>
+										</li>
+										<li>
+											<a href="about.html">L'association</a>
+										</li>
+										<li>
+											<a href="about.html">Nos engagements</a>
+										</li>
+										<li>
+											<a href="about.html">Contact</a>
+										</li>
+
+									</ul>
+								</nav>
+								<!-- eof main nav -->
+								<span class="toggle_menu">
+									<span></span>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<div class="page_toplogo text-center section_padding_top_20">
+				<div class="container">
+					<div class="row bottommargin_15">
+						<div class="col-lg-12">
+							<a href="index.php" class="logo logo_image">
+								<img src="images/logo.png" alt="">
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<h1 class="logo_text">cross
+							<span>power</span>
+						</h1>
+						<p class="text-description">fitness and gym WordPress theme</p>
+					</div>
+				</div>
+			</div>
+
+			<!-- Static -->
+			<div class="intro_section page_mainslider ds">
+				<img src="images/slider/img_slider-01.png" alt="">
+			</div>
+
+			<div class="ls ms">
+				<div class="container">
+					<div class="actualites">
+						<h3 class="actualites-titre">Actualités</h3>
+					</div>
+					<div class="actualites-liste">
+					<div class="row">
+						<div class="col-md-12 col-lg-12">
+
+							<div class="post-type-3">
+								<figure class="post-type-3__img">
+									<a href="index.php#">
+										<img src="images/blog/img_blog-01.jpg" alt="">
+									</a>
+								</figure>
+								<article class="post-type-3__content">
+									<a href="index.php#" class="meta-category">Crossfit & workout</a>
+									<h4 class="post-type-3__title">
+										<a href="index.php#">Ther hand, we denounce with righteus indignation and dislike.</a>
+									</h4>
+									<p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find man.</p>
+									<ul class="meta-post-list">
+										<li>
+											<a href="index.php#" class="meta-post-likes">275</a>
+										</li>
+										<li>
+											<a href="index.php#" class="meta-post-comment">52</a>
+										</li>
+										<li>
+											<span class="meta-post-views">32 575</span>
+										</li>
+									</ul>
+								</article>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+				</div>
+			</div>
+
+            <div class="footer-contact">
+                <div class="container">
+                    <div class="row">
+                        <h3 class="titre-global">Adhésion</h3>
+                        <form method="POST" action="#" class="form-horizontal">
+                            <div class="form-group">
+                                <label class="label-color">Votre nom</label>
+                                <input type="text" class="form-control" placeholder="Nom">
+                            </div>
+                            <div class="form-group">
+                                <label class="label-color">Votre prénom</label>
+                                <input type="text" class="form-control" placeholder="Prénom">
+                            </div>
+                            <div class="form-group">
+                                <label class="label-color">Votre adresse mail</label>
+                                <input type="text" class="form-control" placeholder="Adresse mail">
+                            </div>
+                            <div class="form-group">
+                                <label class="label-color">Votre message</label>
+                                <textarea class="form-control" placeholder="Votre message..."></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary">Envoyer</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="ls ms gallery-01 columns_padding_0">
+                <div class="container">
+                    <div class="actualites">
+                        <h3 class="photo-titre">Photos</h3>
+                    </div>
+                </div>
+
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="isotope_container isotope row masonry-layout" data-filters=".isotope_filters">
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-01.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">boxing</a>
+											<h4 class="gallery-01__title">signup for
+												<br> boxing club</h4>
+											<p class="gallery-01__subtitle">get 50% off</p>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-01">More</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12 gallery-01--bg">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-02.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">services & offers</a>
+											<h4 class="gallery-01__title">What's next on
+												<span>schedule</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-02">our calendar</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute isotope-position col-lg-4 col-md-8 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-03.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">trainers & equipment </a>
+											<h4 class="gallery-01__title">our gym offer first class
+												<span>Trainer and equipment</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-03">get ready</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-04.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">yoga & pilates</a>
+											<h4 class="gallery-01__title">How yoga can
+												<span>change your life</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-03">practice today</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-05.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">products & nutrition</a>
+											<h4 class="gallery-01__title gallery-01__title--color">our top selling
+												<span>products</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-04">shop now</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-06.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">CLOTHES & accessories</a>
+											<h4 class="gallery-01__title gallery-01__title--color">Top brands for
+												<span>best prices</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-04">shop deals</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-07.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">Crossfit & workout</a>
+											<h4 class="gallery-01__title">Start your
+												<span>Training today</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-03">subscribe</a>
+									</div>
+								</div>
+								<div class="isotope-item isotope-position content-absolute col-lg-4 col-md-8 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-08.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">facilities & plans</a>
+											<h4 class="gallery-01__title">get access to all
+												<span>premium facilities</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-03">view details</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12 gallery-01--bg">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-09.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">services & offers</a>
+											<h4 class="gallery-01__title">Calculate Your
+												<span>Body Mass Index</span>
+											</h4>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-02">calculate bmi</a>
+									</div>
+								</div>
+								<div class="isotope-item content-absolute col-lg-2 col-md-4 col-sm-6 col-xs-12">
+									<div class="vertical-item gallery-item">
+										<div class="item-media">
+											<img src="images/gallery/home/img_home-isotope-10.jpg" alt="">
+										</div>
+										<div class="item-content">
+											<a href="index.php#" class="gallery-01__category">gym & fitness</a>
+											<h4 class="gallery-01__title">signup for
+												<span>6 month</span>
+											</h4>
+											<p class="gallery-01__subtitle">get 30% off</p>
+										</div>
+										<a href="index.php#" class="gallery-01__btn-01">More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+			<footer class="page_footer ds ms section_padding_top_60">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4 col-md-4 col-lg-4">
+							<div class="widget social-news-wrapp">
+								<h3 class="widget-title">Réseaux
+									<span>Sociaux</span>
+								</h3>
+								<p class="social-news-text">Rejoignez-nous sur les réseaux sociaux pour nous soutenir</p>
+								<ul class="social-news">
+									<li>
+										<a href="index.php#" class="fa fa-facebook" aria-hidden="true"></a>
+									</li>
+									<li>
+										<a href="index.php#" class="fa fa-twitter" aria-hidden="true"></a>
+									</li>
+									<li>
+										<a href="index.php#" class="fa fa-youtube-play" aria-hidden="true"></a>
+									</li>
+									<li>
+										<a href="index.php#" class="fa fa-google-plus" aria-hidden="true"></a>
+									</li>
+									<li>
+										<a href="index.php#" class="fa fa-vimeo" aria-hidden="true"></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-sm-4 col-md-4 col-lg-4 text-center footer-logo__wrapp">
+							<a href="index.php" class="logo logo_image footer_logo_image">
+								<img src="images/logo_bottom.png" alt="">
+							</a>
+							<p class="logo_text footer_text-logo">cross
+								<span>power</span>
+							</p>
+							<p class="text-description">fitness and gym WordPress theme</p>
+						</div>
+						<div class="col-sm-4 col-md-4 col-lg-4">
+							<div class="widget widget_mailchimp">
+								<h3 class="widget-title">S'inscrire à la
+									<span>Newsletter</span>
+								</h3>
+								<p class="mailchimp-text">Inscrivez-vous pour reçevoir les dernières actualités</p>
+								<form class="signup form-inline" action="http://webdesign-finder.com/" method="get">
+									<div class="form-group">
+										<input name="email" type="email" class="mailchimp_email form-control" placeholder="Indiquez votre adresse e-mail ici...">
+									</div>
+									<button type="submit" class="mailchimp_button">Send</button>
+									<div class="response"></div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+
+			<!-- Copyright -->
+			<div class="page_copyright ls section_padding_20">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6 col-md-6 col-lg-6 text-left">
+							<p class="copyright-info">
+								<a href="index.php#">Association Arcades Vitrolles</a> - Tous droits réservés © <?= date('Y') ?></p>
+						</div>
+						<div class="col-sm-6 col-md-6 col-lg-6 text-right">
+							<p class="copyright-info">Développé
+								<i class="fa fa-heart" aria-hidden="true"></i> par
+								<a href="index.php#">Cyril VELLA - Microvella</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<!-- eof #box_wrapper -->
+	</div>
+	<!-- eof #canvas -->
+
+	<script src="js/main.js"></script>
+	<script src="js/switcher.js"></script>
+
+</body>
+
+</html>
